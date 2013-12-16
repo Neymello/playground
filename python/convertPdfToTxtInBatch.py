75fa103b6df49836f730f3a6610620f81afb2ab5
Ney Mello <neymello@gmail.com>
2013-12-16 16:27:32 -0500

    Improve the convertPdfToTxtInBatch.py script

--- python/convertPdfToTxtInBatch.py
+++ python/convertPdfToTxtInBatch.py
@@ -1,14 +1,26 @@
 import os
 
+####################
+## Uses:PdfMiner 
+## https://pypi.python.org/pypi/pdfminer/
+## Requires Python 2 :-(
+####################
+
+
 count = 0
-for dirname, dirnames, filenames in os.walk('C:\\folder'):
+for dirname, dirnames, filenames in os.walk('folder_name'):
+
 	for f in filenames:
 		fileName = dirname + "\\" +f
+
 		name, ext = os.path.splitext(fileName)
+		name = name
+		fileName = fileName
 
 		if ext == '.pdf':
-			print "python pdf2txt.py -o %s.txt -t text %s" % (name,fileName)	
+			print( r'python pdf2txt.py -o "%s.txt" -t text "%s" ' % (name,fileName) )
 			count+=1
-			os.system("python pdf2txt.py -o %s.txt -t text %s" % (fileName,fileName))
-			
-print count
+			os.system( r'python pdf2txt.py -o "%s.txt" -t text "%s" ' % (name,fileName) )
+	
+
+print(count)
